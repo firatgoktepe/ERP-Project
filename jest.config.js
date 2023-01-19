2fe5cb9c4376374cf4b2734deafcc0d73637c84c
DeveloperFG <firatgoktepe1986@gmail.com>
2023-01-19 17:39:22 +0300

    module changed

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const nextJest = await import('next/jest')
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
