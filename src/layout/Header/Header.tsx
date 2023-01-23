885e9c8cf17a479f899b122424b0b921297a2319
DeveloperFG <firatgoktepe1986@gmail.com>
2023-01-23 18:08:58 +0300

    feat: header component added

---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+
+interface openProps {
+  open: boolean
+  handleDrawerOpen: () => void
+}
+
+const Header: React.FC<openProps> = ({ open, handleDrawerOpen }) => {
+  return (
+    <Toolbar>
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={handleDrawerOpen}
+        edge="start"
+        sx={{
+          marginRight: 5,
+          ...(open && { display: 'none' }),
+        }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" noWrap component="div">
+        Mini variant drawer
+      </Typography>
+    </Toolbar>
+  )
+}
+
+export default Header
