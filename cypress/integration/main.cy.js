5115e21b73336849289c22fca74026cbb0fef1ec
DeveloperFG <firatgoktepe1986@gmail.com>
2023-01-30 14:40:16 +0300

    test: main component & its test modified

---
+++ cypress/integration/main.cy.js
@@ -0,0 +1,11 @@
+// Path: cypress/integration/main.cy.js
+// Test the Main component from layout/Main/Main.tsx
+
+import Main from '../../src/layout/Main/Main.tsx'
+
+describe('<Main />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Main />)
+  })
+})
--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import { DrawerHeader } from '@/layout/Sidebar/DrawerHeader/DrawerHeader'
+import DrawerHeader from '@/layout/Sidebar/DrawerHeader/DrawerHeader'
 
 const Main: React.FC = () => {
   return (
