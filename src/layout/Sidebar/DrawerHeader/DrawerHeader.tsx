569a0f38a11b078b06b514674ac1f2e003e55900
DeveloperFG <firatgoktepe1986@gmail.com>
2023-01-31 17:29:24 +0300

    style: drawer header fixed

--- src/layout/Sidebar/DrawerHeader/DrawerHeader.tsx
+++ src/layout/Sidebar/DrawerHeader/DrawerHeader.tsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles'
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'flex-end',
+  justifyContent: 'space-between',
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
