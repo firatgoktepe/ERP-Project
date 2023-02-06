852f91bf98a064842d53ee884eaf98a3d3e7e149
DeveloperFG <firatgoktepe1986@gmail.com>
2023-02-06 17:18:33 +0300

    test: create test of Error component

---
+++ src/components/common/Example/Spinner/Spinner.test.js
@@ -0,0 +1,12 @@
+//Path: src/components/common/Example/Spinner/Spinner.test.js
+// Test for the Spinner component
+
+import React from 'react'
+import Spinner from './Spinner'
+import { render } from '@testing-library/react'
+
+describe('Spinner', () => {
+  it('renders without crashing', () => {
+    render(<Spinner />)
+  })
+})
