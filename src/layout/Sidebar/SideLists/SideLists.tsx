0d75ffe0ee8dc76e3032ace480e1381dc90b81bb
DeveloperFG <firatgoktepe1986@gmail.com>
2023-02-13 22:07:46 +0300

    fix: modify sidelists & remove its test

--- src/layout/Sidebar/SideLists/SideLists.tsx
+++ src/layout/Sidebar/SideLists/SideLists.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
@@ -7,41 +9,55 @@ import List from '@mui/material/List'
 import Divider from '@mui/material/Divider'
 import InboxIcon from '@mui/icons-material/MoveToInbox'
 import MailIcon from '@mui/icons-material/Mail'
+import { MENU_LIST_ITEMS } from '@/constants/menu-items'
 
 interface openProps {
   open: boolean
 }
 
 const SideLists: React.FC<openProps> = ({ open }) => {
+  const router = useRouter()
+  const initialSelection = MENU_LIST_ITEMS.findIndex(
+    (el) => el.route === router.pathname
+  )
+  const [selectedIndex, setSelectedIndex] = useState(
+    initialSelection !== -1 ? initialSelection : 0
+  )
+
   return (
     <>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem
-            key={text}
-            disablePadding
-            sx={{ display: 'block' }}
-            data-testid="list-item-one"
-          >
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
-                px: 2.5,
-              }}
+        {MENU_LIST_ITEMS.map(({ route, Icon, name }, id) => (
+          <Link href={route} key={id}>
+            <ListItem
+              disablePadding
+              onClick={() => setSelectedIndex(id)}
+              sx={{ display: 'block' }}
+              data-testid="list-item-one"
             >
-              <ListItemIcon
+              <ListItemButton
+                selected={id === selectedIndex}
                 sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
                 }}
               >
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </ListItem>
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                ></ListItemIcon>
+                <Icon />
+                <ListItemText
+                  primary={name}
+                  sx={{ opacity: open ? 1 : 0, textDecoration: 'none' }}
+                />
+              </ListItemButton>
+            </ListItem>
+          </Link>
         ))}
       </List>
       <Divider />
