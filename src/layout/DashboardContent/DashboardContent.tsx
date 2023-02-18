bac2e66d47b704b5dad1c53bd2828603e40473ce
DeveloperFG <firatgoktepe1986@gmail.com>
2023-02-18 18:20:28 +0300

    style: add logic for responsiveness

--- src/layout/DashboardContent/DashboardContent.tsx
+++ src/layout/DashboardContent/DashboardContent.tsx
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography'
 import ActionAreaCard from '@/components/common/Card/ActionAreaCard'
 import { MODULE_LIST_ITEMS } from '@/constants/module-items'
 import { styled } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 const HeaderBox = styled(Box)(({ theme }) => ({
   height: 150,
@@ -17,7 +18,7 @@ const HeaderBox = styled(Box)(({ theme }) => ({
     marginLeft: 15,
   },
   [theme.breakpoints.down('sm')]: {
-    marginLeft: 5,
+    marginLeft: 60,
   },
   borderRadius: '10px',
   backgroundColor: theme.palette.primary.main,
@@ -36,22 +37,40 @@ const CardBox = styled(Box)(({ theme }) => ({
   [theme.breakpoints.down('sm')]: {
     gridTemplateColumns: 'repeat(1, 1fr)',
     height: '20%',
+    marginLeft: 100,
+  },
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: 200,
+    marginRight: 50,
   },
   gap: 10,
   marginTop: 10,
   marginBottom: 50,
-  marginLeft: 100,
-  marginRight: 50,
 }))
 
 const DashboardContent: React.FC = (): JSX.Element => {
+  const matches = useMediaQuery('(max-width:600px)')
   return (
     <>
       <HeaderBox>
-        <Typography variant="h4" sx={{ margin: '0 auto' }}>
+        <Typography
+          variant="h4"
+          sx={{
+            margin: '0 auto',
+            fontSize: matches ? '1.5rem' : '2.125rem',
+            textAlign: matches ? 'center' : 'left',
+          }}
+        >
           Üretim Yönetimi ve Planlama Programı
         </Typography>
-        <Typography variant="h6" sx={{ margin: '0 auto' }}>
+        <Typography
+          variant="h6"
+          sx={{
+            margin: '0 auto',
+            fontSize: matches ? '1rem' : '1.25rem',
+            textAlign: matches ? 'center' : 'left',
+          }}
+        >
           Burada ilgili modül ve işlem menülerini bulabilirsiniz.
         </Typography>
       </HeaderBox>
