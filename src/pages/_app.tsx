8f56dfb468de9b24c3d3cb51b0919d87475af3ad
DeveloperFG <firatgoktepe1986@gmail.com>
2023-02-19 18:36:26 +0300

    style: add all globals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,10 +17,6 @@ export default function App({ Component, pageProps }: AppProps) {
           .drawerTitle {
             margin-left: 10px;
           }
-        `}
-      />
-      <Global
-        styles={css`
           .primaryButton {
             margin-top: 5rem;
             margin-left: 6rem;
