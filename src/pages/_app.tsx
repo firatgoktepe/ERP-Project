0f6331b6fb16ff36508978e1fd9b977b571413bc
DeveloperFG <firatgoktepe1986@gmail.com>
2023-02-19 18:08:14 +0300

    style: add globals dude

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { Provider } from 'react-redux'
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import store from '../redux/store/store'
+import GlobalStyles from '@mui/material/GlobalStyles'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <GlobalStyles
+        styles={{
+          h1: { color: 'red' },
+          h2: { color: 'green' },
+          body: { backgroundColor: 'lightpink' },
+        }}
+      />
       <Component {...pageProps} />
     </Provider>
   )
