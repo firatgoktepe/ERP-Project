21fc7134de04a24df8858172937b3e504103c2d7
DeveloperFG <firatgoktepe1986@gmail.com>
2023-02-19 18:14:05 +0300

    style: add emotion css globals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 import store from '../redux/store/store'
-import GlobalStyles from '@mui/material/GlobalStyles'
+import { Global, css } from '@emotion/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <GlobalStyles
-        styles={{
-          h1: { color: 'red' },
-          h2: { color: 'green' },
-          body: { backgroundColor: 'lightpink' },
-        }}
+      <Global
+        styles={css`
+          .logoImage {
+            border-radius: 50%;
+          }
+        `}
       />
       <Component {...pageProps} />
     </Provider>
