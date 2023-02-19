942d45a1299dfb3e8dd6edf9f4e842fd2470890f
DeveloperFG <firatgoktepe1986@gmail.com>
2023-02-19 18:41:33 +0300

    style: add all global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,23 @@ export default function App({ Component, pageProps }: AppProps) {
     <Provider store={store}>
       <Global
         styles={css`
+          .logoImage {
+            border-radius: 50%;
+          }
+          .logoImage:hover {
+            cursor: pointer;
+          }
           body {
             background-color: lightgray;
+          }
+          .drawerTitle {
+            margin-left: 10px;
+          }
+          .primaryButton {
+            margin-top: 5rem;
+            margin-left: 6rem;
+            width: 15%;
+            text-align: center;
           }
         `}
       />
