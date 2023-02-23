9b6ceb03b584458a62d762d3a8829ee9e21d6be8
DeveloperFG <firatgoktepe1986@gmail.com>
2023-02-23 17:28:30 +0300

    chore: add GlobalCSS into the _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,12 @@
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 import store from '../redux/store/store'
-import { Global, css } from '@emotion/react'
+import GlobalCSS from '@/components/specific/GlobalCSS/GlobalCSS'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <>
-        <Global
-          styles={css`
-            .logoImage {
-              border-radius: 50%;
-            }
-            .logoImage:hover {
-              cursor: pointer;
-            }
-            .drawerTitle {
-              margin-left: 15px !important;
-              font-size: 1.2rem !important;
-            }
-            .primaryButton {
-              marginTop: '5rem',
-              marginLeft: '6rem',
-              width: '15%',
-              textAlign: 'center',
-            }
-          `}
-        />
-      </>
-
+      <GlobalCSS />
       <Component {...pageProps} />
     </Provider>
   )
